65fb24676fb081820eedcc3a8555b91e08b917e4
matthew <mfarrov@g.educaand.es>
2025-05-07 14:02:10 +0200

    Creado una vista para errores 404

---
+++ Skinchase_Laravel/resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex items-center justify-center min-h-screen bg-gray-900 text-white px-4">
+        <div class="text-center">
+            <h1 class="text-3xl font-bold mb-6">This is not available at the moment gng... </h1>
+            <img class="mx-auto max-w-xs rounded shadow-lg" src="{{ asset('images/404.jpg') }}" alt="Maintenance Image">
+        </div>
+    </div>
+@endsection
